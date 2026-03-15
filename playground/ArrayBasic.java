95ea5fce6a8b80790f184fb0a2ddf78dfb754cd8
ahitotsui <atsushiabc1204@gmail.com>
2026-03-15 13:53:58 +0900

    update

--- playground/ArrayBasic.java
+++ playground/ArrayBasic.java
@@ -3,7 +3,23 @@ package playground;
 public class ArrayBasic {
     public static void main(String[] args) {
         //int[] a = new int[2][3];
-        int[][] a = new int[2][3];
+        int[] array0 = {};
+        System.out.println("要素数"+array0.length);
+        try {
+            System.out.println(array0[0]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("0番目の要素がありません"+e);
+        }
+
+
+        int[] array1 = new int[1];
+        System.out.println("要素数"+array1.length);
+        System.out.println(array1[0]);
+
+        int[][] array2 = new int[2][3];
+        System.out.println(array2.length);
+
+
     }
 }
 
